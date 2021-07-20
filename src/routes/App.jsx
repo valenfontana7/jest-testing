8c562c1f5eeed37c123c5d8e82efdbbc9f63d23a
Valentin Fontana <valentin.fontana@dh.com>
2021-07-20 13:56:52 -0300

    asd

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -9,7 +9,11 @@ const App = () => (
   <BrowserRouter>
     <Layout>
       <Switch>
-        <Route exact path="/" component={Home} />
+        <Route
+          exact
+          path="https://valenfontana7.github.io/jest-testing/"
+          component={Home}
+        />
         <Route exact path="/checkout" component={Checkout} />
         <Route component={NotFound} />
       </Switch>
